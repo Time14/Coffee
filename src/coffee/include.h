62fc897704ac333d17ad23472a3bea37ec843310
ed <edvard.thornros@gmail.com>
2016-06-09 11:47:38 +0200

    Managed to get it to build, it was only a tiny error causing a lot of distress upstream.

--- src/coffee/include.h
+++ src/coffee/include.h
@@ -1,21 +1,24 @@
-////////////////////////////////////////////////
-// This file is here to make sure the import  //
-// process is handled smothly. Since the      //
-
+//////////////////////////////////////////////////
+// This file is here to make sure the import	//
+// process is handled smothly. Since the		//
+// OS-s have different include directories.		//
+//////////////////////////////////////////////////
 
 #ifndef __INCLUDE__H__
 #define __INCLUDE__H__
 	//All os-s
-	#if TARGET_OS=="Linux"
+	#if TARGET_OS==LINUX
     	//Linux specific
     	#include <SDL2/SDL.h>
-    	#include <glew.h>
+    	#include <GL/glew.h>
 
-	#elif TARGET_OS=="Windows"
+	#elif TARGET_OS==WINDOWS
 		//Windows specific
     	#include <gl\GL.h>
     	#include "GLEW\glew.h"
 		#include "SDL2\SDL.h"
 
+	#elif TARGET_OS==MAC
+		//Not implemented, yet!
 	#endif
 #endif
